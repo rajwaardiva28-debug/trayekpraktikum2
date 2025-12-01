e1b1eafe3906898e5376f2b5e1c27809e9c46a10
imadsabiq <imadsabiq21@gmail.com>
2025-12-01 17:32:12 +0700

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -35,7 +35,6 @@ elif st.session_state.page == "page2":
     st.title("Praktikum LKD")
     st.write("Silakan pilih modul yang ingin kamu kerjakan:")
 
-    # TOMBOL MODUL 1 – 5
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1: st.button("Modul 1", on_click=lambda: pilih_modul(1))
     with col2: st.button("Modul 2", on_click=lambda: pilih_modul(2))
@@ -46,7 +45,7 @@ elif st.session_state.page == "page2":
     st.write("---")
     st.button("⬅️ Kembali ke halaman awal", on_click=lambda: st.session_state.update(page="page1"))
 
-# --- HALAMAN 3 (MENU LFD - DENGAN DATA CSV) ---
+# --- HALAMAN 3 (MENU LFD - DUA FILE CSV) ---
 elif st.session_state.page == "page3":
     st.title("Praktikum LFD")
     nim_cari = st.session_state.get("input_nim", "").strip()
@@ -61,19 +60,19 @@ elif st.session_state.page == "page3":
             
             # Coba baca file pertama
             try:
-                df1 = pd.read_csv("Sebaran_LFD_FTI2.csv", skiprows=4)
+                df1 = pd.read_csv("sebaranfti1.csv", skiprows=4)
                 df1['NIM'] = df1['NIM'].astype(str) # Pastikan NIM jadi string
                 frames.append(df1)
             except FileNotFoundError:
-                st.warning("⚠️ File 'Sebaran_LFD_FTI2.csv' tidak ditemukan.")
+                st.warning("⚠️ File 'sebaranfti1.csv' tidak ditemukan.")
 
             # Coba baca file kedua
             try:
-                df2 = pd.read_csv("Sebaran_LFD_FTI2.csv", skiprows=4)
+                df2 = pd.read_csv("sebaranfti2.csv", skiprows=4)
                 df2['NIM'] = df2['NIM'].astype(str) # Pastikan NIM jadi string
                 frames.append(df2)
             except FileNotFoundError:
-                st.warning("⚠️ File 'Sebaran_LFD_FTI2.csv' tidak ditemukan.")
+                st.warning("⚠️ File 'sebaranfti2.csv' tidak ditemukan.")
 
             # 2. GABUNGKAN DATA (jika ada file yang berhasil dibaca)
             if frames:
@@ -153,9 +152,9 @@ elif st.session_state.page.startswith("modul_"):
             height=600,
         )
         st.subheader("Jurnal Praktikum")
-        FILE_ID = "1wSQZtgceUIY-HjzbWspSWlK8KkViBtkG"
+        FILE_ID1 = "1wSQZtgceUIY-HjzbWspSWlK8KkViBtkG"
         st.components.v1.html(
-            f'<iframe src="https://drive.google.com/embeddedfolderview?id={FILE_ID}" width="100%" height="100"></iframe>',
+            f'<iframe src="https://drive.google.com/embeddedfolderview?id={FILE_ID1}" width="100%" height="100"></iframe>',
             height=100,
         )
 
